ea1ee773f1229ffe2401dd2aa596ad851611a4ea
la-Alisa <nirononly@mail.ru>
2024-08-26 12:11:14 +0300

    refactor: add commands enum, re-annotate a few classmethod as staticmethod, add try-except block on file reading

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 from typing import Set
+from enum import Enum
 
 
 class TextManager:
@@ -73,53 +74,58 @@ class TextManager:
     def print_control_menu(cls) -> str:
         cls.print_options()
         return input('Ваш выбор: ').lower()
-    
-    @classmethod
-    def print_options(cls) -> None:
+
+    @staticmethod
+    def print_options() -> None:
         print('Выберите действие:\n\n(н) Новая игра\n(в) Выйти')
 
-    @classmethod
-    def get_choice_letter(cls) -> str:
+    @staticmethod
+    def get_choice_letter() -> str:
         return 'Введите букву: '
 
     @classmethod
     def print_gallows(cls, stage: int) -> None:
         print(cls.gallows_stages[stage])
 
-    @classmethod
-    def letter_already_used(cls) -> None:
+    @staticmethod
+    def print_letter_already_used() -> None:
         print('Вы уже вводили эту букву, введите другую')
 
     @classmethod
     def print_used_letter(cls, used: Set[str]) -> None:
         used_letters = ' '.join(used)
         print(f'Использованные буквы: {used_letters}')
-        
+
     @classmethod
     def print_stage(cls, mask: str, stage: int, limit: int,  used: Set[str]) -> None:
         print(mask)
         cls.print_gallows(stage=stage)
-        print(f'Количество ошибок: {stage}, количество допустимых ошибок - {limit-1}')
+        print(
+            f'Количество ошибок: {stage}, количество допустимых ошибок - {limit-1}')
         cls.print_used_letter(used=used)
-    
-    @classmethod
-    def print_hidden_word(cls, word: str) -> None:
+
+    @staticmethod
+    def print_hidden_word(word: str) -> None:
         print(f'Загаданное слово: {word}\n')
-    
-    @classmethod
-    def lose_message(cls) -> None:
-        print('Сожалеем, Вы проиграли!')
-        
-    @classmethod
-    def win_message(cls) -> None:
-        print('Поздравляем! Вы выиграли!')
-    
+
+    @staticmethod
+    def lose_message() -> None:
+        print('\nСожалеем, Вы проиграли!')
+
+    @staticmethod
+    def win_message() -> None:
+        print('\nПоздравляем! Вы выиграли!')
+
+    @staticmethod
+    def file_not_found() -> None:
+        print('Файл с указанным именем не найден. Проверьте правильность названия или целостность файлов.')
+
 
 class Validator:
     alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
-    @classmethod
-    def is_single_char(cls, char: str) -> bool:
+    @staticmethod
+    def is_single_char(char: str) -> bool:
         return len(char) == 1
 
     @classmethod
@@ -127,7 +133,7 @@ class Validator:
         return cls.is_single_char(char=char) and char in cls.alphabet
 
 
-class WordMask:
+class WordManager:
     def __init__(self, word: str) -> None:
         self.__word = word
         self.mask = ['_'] * len(word)
@@ -142,47 +148,51 @@ class WordMask:
         for idx, char in enumerate(self.__word):
             if char == letter:
                 self.mask[idx] = letter
-                
+
     def get_hidden_word(self) -> str:
         return self.__word
 
 
-class Game:
-    def __pick_word(self) -> str:
-        words = []
+class Commands(Enum):
+    START = 'н'
+    END = 'в'
 
-        with open(file='glossary.txt', mode='r', encoding='utf-8') as file:
-            words = file.readlines()
-            
-        return random.choice(seq=words).replace('\n', '')
+
+class Game:
+    def __init__(self, file: str = 'glossary.txt') -> None:
+        self.__file = file
+        self.__words = self.__load_words(file=file)
+
+    def get_words(self) -> list[str]:
+        return self.__words
 
     def play(self) -> None:
         while True:
-            if (inp := TextManager.print_control_menu()) != 'н' and inp != 'в':
+            if (inp := TextManager.print_control_menu()) != Commands.START.value and inp != Commands.END.value:
                 continue
-            elif inp == 'в':
+            elif inp == Commands.END.value:
                 break
-            
+
             mistakes = 0
             mistakes_limit = 6
             used = set()
 
-            word_mask = WordMask(word=self.__pick_word())
+            word_mask = WordManager(word=self.__pick_word())
 
             while mistakes < mistakes_limit and '_' in word_mask.get_mask():
-                TextManager.print_stage(mask=word_mask.get_mask(), stage=mistakes, limit=mistakes_limit, used=used)
+                TextManager.print_stage(mask=word_mask.get_mask(
+                ), stage=mistakes, limit=mistakes_limit, used=used)
 
                 letter = ''
                 while True:
                     letter = input(TextManager.get_choice_letter()).lower()
 
                     if letter in used:
-                        TextManager.letter_already_used()
+                        TextManager.print_letter_already_used()
                         continue
-                    
+
                     if Validator.is_valid_char(letter):
                         break
-
 
                 if word_mask.is_letter_in_word(letter=letter):
                     word_mask.reveal_letter(letter=letter)
@@ -190,14 +200,24 @@ class Game:
                     mistakes += 1
 
                 used.add(letter)
-                
+
             if mistakes == mistakes_limit:
                 TextManager.lose_message()
             else:
                 TextManager.win_message()
-                
+
             TextManager.print_hidden_word(word=word_mask.get_hidden_word())
-                    
+
+    def __load_words(self, file: str) -> list[str]:
+        try:
+            with open(file=file, mode='r', encoding='utf-8') as file:
+                return file.readlines()
+        except FileNotFoundError:
+            TextManager.file_not_found()
+            exit(1)
+
+    def __pick_word(self) -> str:
+        return random.choice(seq=self.get_words()).replace('\n', '')
 
 
 if __name__ == '__main__':

108c81a9940777ca1f8f1cfab6cdec7a124b7819
la-Alisa <nirononly@mail.ru>
2024-08-30 23:42:53 +0300

    refactor: remove TextManager, Validator clasees, add GameConfig, reorginize game logic

--- main.py
+++ main.py
@@ -1,144 +1,29 @@
 import random
-from typing import Set
 from enum import Enum
 
-
-class TextManager:
-    gallows_stages = [
-        """
-            -----
-            |   |
-                |
-                |
-                |
-                |
-            --------\n
-        """,
-        """
-            -----
-            |   |
-            O   |
-                |
-                |
-                |
-            --------\n
-        """,
-        """
-            -----
-            |   |
-            O   |
-            |   |
-                |
-                |
-            --------\n
-        """,
-        """
-            -----
-            |   |
-            O   |
-           /|   |
-                |
-                |
-            --------\n
-        """,
-        """
-            -----
-            |   |
-            O   |
-           /|\\ |
-                |
-                |
-            --------\n
-        """,
-        """
-            -----
-            |   |
-            O   |
-           /|\\ |
-           /    |
-                |
-            --------\n
-        """,
-        """
-            -----
-            |   |
-            O   |
-           /|\\ |
-           / \\ |
-                |
-            --------\n
-        """,
-    ]
-
-    @staticmethod
-    def print_options() -> None:
-        print('Выберите действие:\n\n(н) Новая игра\n(в) Выйти')
-
-    @staticmethod
-    def get_choice_letter() -> str:
-        return 'Введите букву: '
-
-    @staticmethod
-    def get_menu_choice() -> str:
-        return 'Ваш выбор: '
-
-    @classmethod
-    def print_gallows(cls, stage: int) -> None:
-        print(cls.gallows_stages[stage])
-
-    @staticmethod
-    def print_letter_already_used() -> None:
-        print('Вы уже вводили эту букву, введите другую')
-
-    @classmethod
-    def print_used_letter(cls, used: Set[str]) -> None:
-        used_letters = ' '.join(used)
-        print(f'Использованные буквы: {used_letters}')
-
-    @classmethod
-    def print_stage(cls, mask: str, stage: int, limit: int,  used: Set[str]) -> None:
-        print(mask)
-        cls.print_gallows(stage=stage)
-        print(
-            f'Количество ошибок: {stage}, количество допустимых ошибок - {limit-1}')
-        cls.print_used_letter(used=used)
-
-    @staticmethod
-    def print_hidden_word(word: str) -> None:
-        print(f'Загаданное слово: {word}\n')
-
-    @staticmethod
-    def print_lose() -> None:
-        print('\nСожалеем, Вы проиграли!')
-
-    @staticmethod
-    def print_win() -> None:
-        print('\nПоздравляем! Вы выиграли!')
-
-    @staticmethod
-    def print_file_not_found() -> None:
-        print('Файл с указанным именем не найден. Проверьте правильность названия или целостность файлов.')
-
-
-class Validator:
-    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-
-    @staticmethod
-    def is_single_char(char: str) -> bool:
-        return len(char) == 1
-
-    @classmethod
-    def is_valid_char(cls, char: str) -> bool:
-        return cls.is_single_char(char=char) and char in cls.alphabet
+from text_manager import GameTextManager, get_text_manager_factory
+from validators import ValidatorManager
 
 
 class WordManager:
-    def __init__(self, word: str) -> None:
+    def __init__(self, word: str, mask_symbol: str = '_') -> None:
         self.__word = word
-        self.mask = ['_'] * len(word)
+        self.mask_symbol = mask_symbol
+        self.mask = [mask_symbol] * len(word)
 
     def get_mask(self) -> str:
         return ' '.join(self.mask)
+
+    @property
+    def mask_symbol(self) -> str:
+        return self._mask_symbol
+
+    @mask_symbol.setter
+    def mask_symbol(self, symbol: str) -> None:
+        self._mask_symbol = symbol
+
+    def is_reveal_word(self) -> bool:
+        return self.mask_symbol not in self.mask
 
     def is_letter_in_word(self, letter: str) -> bool:
         return letter in self.__word
@@ -152,77 +37,140 @@ class WordManager:
         return self.__word
 
 
-class Commands(Enum):
-    START = 'н'
-    END = 'в'
+class MainCommands(Enum):
+    START = '1'
+    CHANGE_LANGUAGE = '2'
+    END = '3'
+
+
+class LangCommands(Enum):
+    RU = '1'
+    EN = '2'
+
+
+class GameManager:
+    def __init__(self, file: str = 'glossary\\ru.txt') -> None:
+        self.file = file
+
+    @property
+    def filename(self) -> str:
+        return self.file
+
+    @filename.setter
+    def filename(self, lang: str) -> None:
+        self.file = f'glossary\\{lang}.txt'
+
+    def _load_words(self, text_manager: GameTextManager) -> list[str]:
+        try:
+            with open(file=self.file, mode='r', encoding='utf-8') as file:
+                return file.readlines()
+        except FileNotFoundError:
+            text_manager.print_file_not_found()
+            exit(1)
+        except Exception as ex:
+            print(ex)
+            exit(-1)
+
+    def _pick_word(self, text_manager: GameTextManager) -> str:
+        return random.choice(seq=self._load_words(text_manager=text_manager)).replace('\n', '')
+
+    def change_language(self, text_manager: GameTextManager, validator: ValidatorManager) -> None:
+        text_manager.print_available_languages()
+
+        selected = ''
+        while True:
+            match text_manager.input_text_manager.get_chosen_option():
+                case LangCommands.EN.value:
+                    selected = LangCommands.EN.name.lower()
+                    break
+                case LangCommands.RU.value:
+                    selected = LangCommands.RU.name.lower()
+                    break
+                case _:
+                    text_manager.print_wrong_command()
+
+        text_manager.change_language(lang=selected)
+        validator.change_language(lang=selected)
+        self.filename = selected
+
+    def start_game(self, text_manager: GameTextManager, word_manager: WordManager, validator: ValidatorManager) -> None:
+        mistakes = 0
+        mistakes_limit = 6
+        used = set()
+
+        while mistakes < mistakes_limit and not word_manager.is_reveal_word():
+            text_manager.print_stage(
+                stage=mistakes, limit=mistakes_limit, used=used, mask=word_manager.get_mask())
+
+            letter = ''
+            while True:
+                letter = text_manager.get_letter()
+
+                if not validator.validate(letter=letter):
+                    continue
+
+                if letter in used:
+                    text_manager.print_letter_already_used()
+                else:
+                    break
+
+            if word_manager.is_letter_in_word(letter=letter):
+                word_manager.reveal_letter(letter=letter)
+            else:
+                mistakes += 1
+
+            used.add(letter)
+
+        is_win = mistakes != mistakes_limit
+
+        text_manager.print_game_result(
+            word=word_manager.get_hidden_word(), is_win=is_win)
+
+
+class GameConfig:
+    def __init__(self, mistakes_limit: int = 6, mask_symbol: str = '_') -> None:
+        self.limit = mistakes_limit
+        self._mask_symbol = mask_symbol
+
+    @property
+    def mistakes_limit(self) -> int:
+        return self.limit
+
+    @mistakes_limit.setter
+    def mistakes_limit(self, limit: int) -> None:
+        self.limit = limit
+
+    @property
+    def mask_symbol(self) -> str:
+        return self._mask_symbol
+
+    @mask_symbol.setter
+    def mask_symbol(self, symbol: str) -> None:
+        self._mask_symbol = symbol
 
 
 class Game:
-    def __init__(self, file: str = 'glossary.txt') -> None:
-        self.__file = file
-        self.__words = self.__load_words(file=file)
-
-    def get_words(self) -> list[str]:
-        return self.__words
-
     def play(self) -> None:
-        while True:
-            TextManager.print_options()
+        text_manager = GameTextManager(
+            factory=get_text_manager_factory(lang='ru'))
+        game_manager = GameManager()
+        validator = ValidatorManager()
 
-            match (inp := input(TextManager.get_menu_choice()).lower()):
-                case Commands.START.value:
-                    pass
-                case Commands.END.value:
+        while True:
+            text_manager.print_menu()
+            inp = text_manager.get_menu_option()
+
+            match inp:
+                case MainCommands.START.value:
+                    game_manager.start_game(text_manager=text_manager, word_manager=WordManager(
+                        word=game_manager._pick_word(text_manager=text_manager)), validator=validator)
+                case MainCommands.CHANGE_LANGUAGE.value:
+                    game_manager.change_language(
+                        text_manager=text_manager, validator=validator)
+                case MainCommands.END.value:
                     break
                 case _:
-                    continue
-                
-            word_manager = WordManager(word=self.__pick_word())
-
-            mistakes = 0
-            mistakes_limit = 6 if len(word_manager.get_hidden_word()) <= 10 else 8
-            used = set()
-
-
-            while mistakes < mistakes_limit and '_' in word_manager.get_mask():
-                TextManager.print_stage(mask=word_manager.get_mask(
-                ), stage=mistakes, limit=mistakes_limit, used=used)
-
-                letter = ''
-                while True:
-                    letter = input(TextManager.get_choice_letter()).lower()
-                    
-                    if Validator.is_valid_char(letter):
-                        break
-
-                    if letter in used:
-                        TextManager.print_letter_already_used()
-                        continue
-
-                if word_manager.is_letter_in_word(letter=letter):
-                    word_manager.reveal_letter(letter=letter)
-                else:
-                    mistakes += 1
-
-                used.add(letter)
-
-            if mistakes == mistakes_limit:
-                TextManager.print_lose()
-            else:
-                TextManager.print_win()
-
-            TextManager.print_hidden_word(word=word_manager.get_hidden_word())
-
-    def __load_words(self, file: str) -> list[str]:
-        try:
-            with open(file=file, mode='r', encoding='utf-8') as file:
-                return file.readlines()
-        except FileNotFoundError:
-            TextManager.print_file_not_found()
-            exit(1)
-
-    def __pick_word(self) -> str:
-        return random.choice(seq=self.get_words()).replace('\n', '')
+                    text_manager.print_wrond_command()
 
 
 if __name__ == '__main__':
